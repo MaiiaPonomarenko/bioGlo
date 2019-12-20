978433586469c00208af2315b309366badae6d61
Maiia Po <nedopyokish@gmail.com>
2019-12-21 01:47:39 +0500

    accordion

--- js/script.js
+++ js/script.js
@@ -11,16 +11,37 @@ const getPopup = () => {
       popupCall.style.display = 'block';
     })
   });
-  popupClose.addEventListener('click', (event) => {
+
+  popupCall.addEventListener('click', (event) => {
     let target = event.target;
     if (target.classList.contains('popup-close'))
       popupCall.style.display = 'none';
     else{
-      target = target.closest('.popup-dialog');
+      target = target.closest('.popup-content');
       if (!target)
         popupCall.style.display = 'none';
     }
-    
-  })
+  });
 };
 getPopup();
+
+/***** accordion *****/
+const accordion = () => {
+  const headingOneTwo = document.querySelector('#headingOne-two'),
+        collapseOnTwo = document.querySelector('#collapseOne-two'),
+        headingTwoTwo = document.querySelector('#headingTwo-two'),
+        collapseTwoTwo = document.querySelector('#collapseTwo-two'),
+        headingThreeTwo = document.querySelector('#headingThree-two'),
+        collapseThreeTwo = document.querySelector('#collapseThree-two');
+  
+  headingOneTwo.addEventListener('click', () => {
+    collapseOnTwo.classList.toggle('in');
+  });
+  headingTwoTwo.addEventListener('click', () => {
+    collapseTwoTwo.classList.toggle('in');
+  });
+  headingThreeTwo.addEventListener('click', () => {
+    collapseThreeTwo.classList.toggle('in');
+  });
+};
+accordion();
