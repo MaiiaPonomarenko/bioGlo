80fad4dc1856862e9c47c68bc43c4ea2c1b13fd6
Maiia Po <nedopyokish@gmail.com>
2019-12-21 14:12:51 +0500

    + popup-discount

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-/***** popup *****/
-const getPopup = () => {
+/***** popup popup-call *****/
+const getPopupCall = () => {
   const callBtn = document.querySelectorAll('.call-btn'),
         popupCall = document.querySelector('.popup-call');
   
@@ -22,16 +22,40 @@ const getPopup = () => {
     }
   });
 };
-getPopup();
+getPopupCall();
+
+/***** popup popup-discount *****/
+const getPopupDiscount = () => {
+  const discountBtn = document.querySelectorAll('.discount-btn'),
+        popupDiscount = document.querySelector('.popup-discount');
+  
+  discountBtn.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      popupDiscount.style.display = 'block';
+    })
+  });
+  
+  popupDiscount.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.contains('popup-close'))
+      popupDiscount.style.display = 'none';
+    else{
+      target = target.closest('.popup-content');
+      if (!target)
+        popupDiscount.style.display = 'none';
+    }
+  });
+};
+getPopupDiscount();
 
 /***** accordion *****/
 const accordion = () => {
-  const headingOneTwo = document.querySelector('#headingOne-two'),
-        collapseOnTwo = document.querySelector('#collapseOne-two'),
-        headingTwoTwo = document.querySelector('#headingTwo-two'),
-        collapseTwoTwo = document.querySelector('#collapseTwo-two'),
-        headingThreeTwo = document.querySelector('#headingThree-two'),
-        collapseThreeTwo = document.querySelector('#collapseThree-two');
+  const headingOneTwo = document.getElementById('headingOne-two'),
+        collapseOnTwo = document.getElementById('collapseOne-two'),
+        headingTwoTwo = document.getElementById('headingTwo-two'),
+        collapseTwoTwo = document.getElementById('collapseTwo-two'),
+        headingThreeTwo = document.getElementById('headingThree-two'),
+        collapseThreeTwo = document.getElementById('collapseThree-two');
   
   headingOneTwo.addEventListener('click', () => {
     collapseOnTwo.classList.toggle('in');
