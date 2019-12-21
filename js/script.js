56bf8bb7aa685ecae75e24ed9bd5f3d4f5076672
Maiia Po <nedopyokish@gmail.com>
2019-12-21 15:02:18 +0500

    + кнопка "Больше"

--- js/script.js
+++ js/script.js
@@ -69,3 +69,20 @@ const accordion = () => {
   });
 };
 accordion();
+
+/***** more *****/
+const more = () => {
+  const addSentenceBtn = document.querySelector('.add-sentence-btn'),
+        itemSentence = document.querySelectorAll('.item-sentence');
+  
+  addSentenceBtn.addEventListener('click', () => {
+    itemSentence.forEach((item) => {
+      if (item.classList.contains('hidden') || item.classList.contains('visible-sm-block')){
+        item.classList.remove('visible-sm-block');
+        item.classList.remove('hidden');
+        addSentenceBtn.style.display = 'none';
+      }
+    })
+  });
+};
+more();
